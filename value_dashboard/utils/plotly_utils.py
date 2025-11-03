d21097d2ce6a59aa3940b8b402b9ac31981a0e0f
Sen, Grigoriy <grigoriy.sen@pega.com>
2025-11-03 09:43:03 +0100

    Theme updates

--- value_dashboard/utils/plotly_utils.py
+++ value_dashboard/utils/plotly_utils.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 
 import plotly.graph_objects as go
 import plotly.io as pio
+import streamlit as st
 
 
 @lru_cache(maxsize=1)
@@ -13,7 +14,21 @@ def init_plotly_theme():
         "steelblue", "lightgreen", "lightblue", "#fffacd"
     ]
 
-    base = pio.templates.default  # pio.templates["plotly_white"]
-    new_template = pio.templates[base].update(layout=go.Layout(colorway=adjusted_colors))
+    dark_palette = [
+        "#3498DB", "#2ECC71", "#E74C3C", "#F39C12", "#9B59B6",
+        "#1ABC9C", "#D35400", "#4682B4", "#4B006E", "#7B3F00",
+        "#FFFF00", "#ADD8E6"
+    ]
+
+    light_palette = [
+        "#2C82C9", "#27AE60", "#D64541", "#8E44AD", "#E67E22",
+        "#16A085", "#2C3E50", "#B9770E", "#4B006E", "#1F618D",
+        "#117A65", "#8B0000"
+    ]
+
+    base = pio.templates.default
+    base_st = st.get_option("theme.base") or "light"
+    colorway = dark_palette if base_st == "dark" else light_palette
+    new_template = pio.templates[base].update(layout=go.Layout(colorway=colorway))
     pio.templates["cdhvd"] = new_template
     pio.templates.default = "cdhvd"
