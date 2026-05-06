1cda55bc309f672cfd436d333e0b839b9fde16fe
Grigoriy Sen <grigoriy.sen@pega.com>
2026-05-06 21:41:46 +0200

    Decouple config editor from optional AI dependencies

--- value_dashboard/config_generator/__init__.py
+++ value_dashboard/config_generator/__init__.py
@@ -1,17 +1,14 @@
-from value_dashboard.config_generator.ai import build_ai_config_prompt, generate_ai_sections
 from value_dashboard.config_generator.configuration_studio import render_configuration_studio
 from value_dashboard.config_generator.preprocess import apply_ih_preprocessing, build_ih_config, \
     build_schema_preview, compile_calculated_fields, compile_filter_rules, detect_ih_file_settings, load_ih_sample
 
 __all__ = [
     "apply_ih_preprocessing",
-    "build_ai_config_prompt",
     "build_ih_config",
     "build_schema_preview",
     "compile_calculated_fields",
     "compile_filter_rules",
     "detect_ih_file_settings",
-    "generate_ai_sections",
     "load_ih_sample",
     "render_configuration_studio"
 ]
